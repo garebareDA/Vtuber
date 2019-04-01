6c6810fe0c7fabefcd63baffd6cfec8f8a40c281
gann <daiki_17n1100521@nnn.ed.jp>
2019-04-01 22:44:08 +0900

    first

---
+++ model/瞬き.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 瞬き : MonoBehaviour {
+
+    // Use this for initialization
+    private SkinnedMeshRenderer blendshape_SMR;
+
+    // Use this for initialization
+    void Start()
+    {
+        blendshape_SMR = this.GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        StartCoroutine("start");
+
+    }
+
+    IEnumerator start()
+    {
+        while (true)
+        {
+            float _seed = Random.Range(0.0f, 200.0f);
+
+            if (_seed > 0.7f)
+            {
+                blendshape_SMR.SetBlendShapeWeight(0, -50.0f + -50.0f * Mathf.Sin(Time.time));
+            }else{
+                blendshape_SMR.SetBlendShapeWeight(0, 0f + 1000.0f * Mathf.Sin(Time.time));
+                
+            }
+
+            yield return new WaitForSeconds(10000);
+        }
+        
+    }
+}
+
+
+
+
+
+
---
+++ socketServer/soserver.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+
+
+public class soserver : MonoBehaviour {
+
+    WebSocket ws;
+    Rotete rotate;  
+   
+    // Use this for initialization
+    void Start () {
+        Transform transform = this.transform;
+   
+        ws = new WebSocket("ws://localhost:8080");
+        ws.Connect();
+        ws.OnMessage += (object sender, MessageEventArgs e) => {
+            rotate = JsonUtility.FromJson<Rotete>(e.Data);
+        };
+        ws.Send("connect");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.localEulerAngles = new Vector3(rotate.z, rotate.x, rotate.y);
+    }
+}
+
+public class Rotete
+{
+    public float z;
+    public float x;
+    public float y;
+}
